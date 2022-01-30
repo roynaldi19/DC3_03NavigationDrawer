97123a76c6a3464d9e13356bf6fd6365078833f9
roynaldi19 <roynaldi19@gmail.com>
2022-01-30 18:44:05 +0700

    Initial commit

---
+++ app/src/main/java/com/roynaldi19/bfaa03_03navigationdrawer/ui/slideshow/SlideshowViewModel.kt
@@ -0,0 +1,13 @@
+package com.roynaldi19.bfaa03_03navigationdrawer.ui.slideshow
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class SlideshowViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is slideshow Fragment"
+    }
+    val text: LiveData<String> = _text
+}
